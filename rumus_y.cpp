db8d8964992b4755166b5f9f5bc04901e7ad9729
DaffaErindaZiddana <114640090+DaffaErindaZiddana@users.noreply.github.com>
2022-10-24 06:07:57 +0700

    Pengumpulan tugas 3

---
+++ rumus_y.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    double y, b = 25, x = 15, c = 20;
+
+    cout << "Mencari Y = b.x^2 + 0,5.x-c" << endl;
+    cout << endl;
+
+    y = (b * (x * x)) + (0.5 * x) - c;
+
+    cout << "Hasil Y = " << y << endl;
+
+
+
+
+    return 0;
+}
